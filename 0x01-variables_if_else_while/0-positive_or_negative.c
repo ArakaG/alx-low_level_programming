5e9fe9d5489568c0189992886b94f25bb9f1fd01
Gift Ooga <giftooga95@gmail.com>
2023-10-23 16:02:13 -0700

    upp

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - Prints if number is positive, zero or negative
- * Return : Always (Success)
+ * main - Entry point
+ * Return : Always 0 (Success)
  */
 
 int main(void)
