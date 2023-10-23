e682b8d398e11fc22f080f3961ca02c72913ccff
Gift Ooga <giftooga95@gmail.com>
2023-10-23 16:12:32 -0700

    upp

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,11 +2,12 @@
 
 /**
  * main - Entry point
- * Return - Always 0
+ * Return - Always 0 (Success)
  */
 
 int main(void)
 {
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+check the code
+Return: Always 0
+
+main(void)
+{
+	print_alphabet();
+	return (0);
+}
