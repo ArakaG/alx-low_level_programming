8ff20ffed9ca980bff03d722fabcdf291b2bb22b
Gift Ooga <giftooga95@gmail.com>
2023-10-23 16:57:50 -0700

    up

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,9 +2,9 @@
 
 /**
  * main - Entry point
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(void)/*Found in the header file*/
 {
 	char c;
 	int i = 0;
