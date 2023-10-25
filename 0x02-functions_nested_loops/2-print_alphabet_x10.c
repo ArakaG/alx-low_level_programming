9fc8d2e5ad2bcf05ad8d44ee6e30d31b68b98a8c
Gift Ooga <giftooga95@gmail.com>
2023-10-24 17:15:24 -0700

    Up

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet_x10 - Prints alphabets ten times
  * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void)/*Found in the header file*/
+
+void print_alphabet_x10(void)
 {
 	char c;
 	int i = 0;
